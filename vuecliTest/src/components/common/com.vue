c844ace3987dfb12dfeb01c7b92f04c58c0e2607
liulijun <11141736405@qq.com>
2017-12-04 13:00:42 +0800

    添加模块

---
+++ vuecliTest/src/components/common/com.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="com">
+     我是一个公共组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'com',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+   
+</style>
--- vuecliTest/src/components/page/vip.vue
+++ vuecliTest/src/components/page/vip.vue
@@ -1,8 +1,18 @@
 <template>
+    <div id="app">
+
 <h1>会员中心</h1>
+<com></com>
+</div>
 </template>
 <script>
-
+import com from '@/components/common/com'
+export default {
+  name: 'app',
+  components:{
+    com
+  }
+}
 </script>
 <style>
 
